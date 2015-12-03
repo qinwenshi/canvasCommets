9289a7aaeaa68569af7f749268431a0f36904dbe
Wen <shiqinwen@wondersgroup.com>
2015-12-03 17:53:06 +0800

    magic

--- public/javascripts/comment_code.js
+++ public/javascripts/comment_code.js
@@ -2,11 +2,12 @@
  window.initCanvas = function(option){
 	var img = new Image();
 	var qiniuServer = 'http://7xov9r.com1.z0.glb.clouddn.com/',
-	qiniuPicCompress = '?imageView/2/w/1600/h/800';
+	qiniuPicCompress = '?imageView/2/w/1600/h/700';
 	imgId = option.imgId;
 	imgName = option.imgName;
 	img.src = qiniuServer +imgName+'.jpg'+qiniuPicCompress;
-
+    imgWidth = option.width || '970';
+    imgHeight = option.height || '650';
 	img.id = imgId;
     $('<div id = "preViewPic" class="preViewPic"/>').appendTo($('.container'));
     
@@ -26,10 +27,10 @@
 
 	function createContainer(){
 		$('#preViewPic').append(initContainer({
-			id     :img.id, 
-    		picUrl: img.src,
-    		width  :'970',
-    		height :'650',
+			id     : img.id, 
+    		picUrl : img.src,
+    		width  : imgWidth,
+    		height : imgHeight,
     		comments: savedComments
 		}));  
 	}
