aeb3dba88fd84772198a4c59e9a2706435336706
Wen <shiqinwen@wondersgroup.com>
2015-12-18 15:02:01 +0800

    x

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', routes);
 app.use('/users', users);
 
-// catch 404 and forward to error handlers
+// catch 404 and forward to error_handlers
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
